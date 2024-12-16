b0450cb668df01dac10722d31570b8917581cdff
brigittemendez1302 <u201914955@upc.edu.pe>
2024-12-16 01:58:16 -0500

    add: updated authcontroller using custom and handle error

--- src/auth/infrastructure/controllers/AuthController.ts
+++ src/auth/infrastructure/controllers/AuthController.ts
@@ -1,27 +1,42 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { AuthService } from '../../application/AuthService';
 import { DynamoAuthRepository } from '../repositories/DynamoAuthRepository';
+import { CustomError, handleError } from '../../../utils/errorHandler';
 
 const authService = new AuthService(new DynamoAuthRepository());
 
 export const signup: APIGatewayProxyHandler = async (event) => {
   const { username, password } = JSON.parse(event.body || '{}');
   try {
+    if (!username || !password) {
+      throw new CustomError("El nombre de usuario y la contraseña son obligatorios.", 400);
+    }
+
     await authService.signup(username, password);
-    return { statusCode: 201, body: JSON.stringify({ message: 'Usuario registrado exitosamente.' }) };
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ message: 'Usuario registrado exitosamente.' }),
+    };
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : 'Ocurrió un error desconocido';
-    return { statusCode: 400, body: JSON.stringify({ message: errorMessage }) };
+    return handleError(error);
   }
 };
 
 export const login: APIGatewayProxyHandler = async (event) => {
   const { username, password } = JSON.parse(event.body || '{}');
   try {
+    if (!username || !password) {
+      throw new CustomError("El nombre de usuario y la contraseña son obligatorios.", 400);
+    }
+
     const token = await authService.login(username, password);
-    return { statusCode: 200, body: JSON.stringify({ token }) };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ token }),
+    };
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : 'Ocurrió un error desconocido';
-    return { statusCode: 401, body: JSON.stringify({ message: errorMessage }) };
+    return handleError(error);
   }
 };
